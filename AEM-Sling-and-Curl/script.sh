637894fb86215af9e5e03faf1628eb826cc87012
Trincu, Dan <dan.trincu@pearson.com>
2022-01-12 17:09:48 +0200

    Final Task added, but I still need to modify some things

--- AEM-Sling-and-Curl/script.sh
+++ AEM-Sling-and-Curl/script.sh
@@ -1,14 +1,13 @@
-!#/bin/bash
+#!/bin/bash
+
 backstop reference
 
-curl 'http://localhost:4502/crx/server/crx.default/jcr%3aroot' \
-  -H 'Content-Type: multipart/form-data; boundary=crxde' \
-  -u admin:admin \
-  --data-raw $'--crxde\r\nContent-Disposition: form-data; name=":diff"\r\nContent-Type: text/plain; \
-  charset=utf-8\r\n\r\n^/content/platform/ccl/en/components/jcr:content/par-main/title/jcr:title : "Components - Sling"\r\n--crxde-- '
+curl -s -u admin:admin \
+  -Fjcr:title="Append" \
+  http://localhost:4502/content/platform/ccl/en/components/jcr:content
 
-curl 'http://localhost:4502/crx/de/replication.jsp' \
-  --data-raw 'path=%2Fcontent%2Fplatform%2Fccl%2Fen%2Fcomponents%2Fjcr%3Acontent%2Fpar-main%2Ftitle&action=replicate&_charset_=utf-8' \
-  -u admin:admin
+curl -s -u admin:admin \
+  -X POST -F path="/content/platform/ccl/en/components" -F \
+  cmd="activate" http://localhost:4502/bin/replicate.json
 
 backstop test
---
+++ Final-Task/final.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+AEMpath="/Users/datr1605/repos/aem-core"
+URL="http://localhost:4502/crx/packmgr/service/.json"
+
+# BUILD AEM-CORE-APPLICATION
+cd "$AEMpath"/aem-core-application/ && mvn clean package -P6_4;
+
+# BUILD AEM-CONFIG
+echo -e "\n\n"
+read -n1 -rp "Do you want to BUILD aem-configuration-goldline ? [Y/n] : " response; echo -e "\n"
+if [[ "$response" =~ ^(y|Y)$ ]]; then
+  cd "$AEMpath"/aem-configurations/aem-configuration-goldline/ && mvn clean package -P6_4 #optional
+  configBuilt=1
+fi
+
+# BUILD DEMO-SITES-CONTENT
+echo -e "\n\n"
+read -n1 -rp "Do you want to BUILD demo-sites-content ? [Y/n] : " response; echo -e "\n"
+if [[ "$response" =~ ^(y|Y)$ ]]; then
+  cd "$AEMpath"/demo-sites-content/ && mvn clean package -P6_4 #optional
+  contentBuilt=1
+fi
+
+# BACKSTOP REFERENCE
+backstop reference
+
+# UPLOAD / INSTALL AEM-CORE-APPLICATION
+curl -s -u admin:admin \
+  -F package=@"$AEMpath"/aem-core-application/target/aem-core-application-5.0.1558-SNAPSHOTaem64.zip "$URL"/\?cmd\=upload
+  if test $? -ne 0; then
+    echo "Upload failed."
+  fi
+
+curl -s -u admin:admin \
+  -X POST "$URL"/etc/packages/com.pearson.aem/aem-core-applicationaem64-5.0.1558-SNAPSHOT.zip\?cmd\=install
+  if test $? -ne 0; then
+    echo "Install failed."
+  fi
+
+# UPLOAD / INSTALL AEM-CONFIG -- OPTIONAL
+if [ $configBuilt -eq 1 ]; then
+  echo -e "\n\n"
+  read -n1 -rp "Do you want to UPLOAD aem-configuration-goldline ? [Y/n] : " response; echo -e "\n"
+  if [[ "$response" =~ ^(y|Y)$ ]]; then
+    curl -s -u admin:admin \
+      -F package=@"$AEMpath"/aem-configurations/aem-configuration-goldline/target/aem-configuration-gold-1.0.1294-SNAPSHOT.zip "$URL"/\?cmd\=upload
+    configUploaded=1
+    if test $? -ne 0; then
+      echo "Upload failed."
+    fi
+  fi
+fi
+
+if [ $configBuilt -eq 1 ] && [ $configUploaded -eq 1 ]; then
+  echo -e "\n\n"
+  read -n1 -rp "Do you want to INSTALL aem-configuration-goldline ? [Y/n] : " response; echo -e "\n"
+  if [[ "$response" =~ ^(y|Y)$ ]]; then
+    curl -s -u admin:admin \
+      -X POST "$URL"/etc/packages/com.pearson.aem/aem-configuration-gold-1.0.1294-SNAPSHOT.zip\?cmd\=install
+    if test $? -ne 0; then
+      echo "Install failed."
+    fi
+  fi
+fi
+
+# UPLOAD / INSTALL DEMO-SITES-CONTENT -- OPTIONAL
+if [ $contentBuilt -eq 1 ]; then
+  echo -e "\n\n"
+  read -n1 -rp "Do you want to UPLOAD demo-sites-content ? [Y/n] : " response; echo -e "\n"
+  if [[ "$response" =~ ^(y|Y)$ ]]; then
+    curl -s -u admin:admin \
+      -F package=@"$AEMpath"/demo-sites-content/target/demo-sites-content-2.0.970-SNAPSHOT.zip "$URL"/\?cmd\=upload
+    contentUploaded=1
+    if test $? -ne 0; then
+      echo "Upload failed."
+    fi
+  fi
+fi
+
+if [ $contentBuilt -eq 1 ] && [ $contentUploaded -eq 1 ]; then
+  echo -e "\n\n"
+  read -n1 -rp "Do you want to INSTALL demo-sites-content ? [Y/n] : " response; echo -e "\n"
+  if [[ "$response" =~ ^(y|Y)$ ]]; then
+    curl -s -u admin:admin \
+    -X POST "$URL"/etc/packages/com.pearson.aem/demo-sites-content-2.0.970-SNAPSHOT.zip\?cmd\=install
+    if test $? -ne 0; then
+        echo "Install failed."
+    fi
+  fi
+fi
+
+# BACKSTOP LATEST
+backstop latest
+
+# FALCON TEST 
+
+cd "$AEMpath"/aem-falcon-tests/ && mvn clean verify -Pgold_dev,all_sites -Dcucumber.option="--tags @PMCCP-9405"
