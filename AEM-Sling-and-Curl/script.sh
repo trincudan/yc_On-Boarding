061f0fd2bc1d0c910e09694c284f8d46864f7aa6
Trincu, Dan <dan.trincu@pearson.com>
2022-01-18 11:39:11 +0200

    Updated AEM-Sling-and-Curl script

--- AEM-Sling-and-Curl/script.sh
+++ AEM-Sling-and-Curl/script.sh
@@ -2,12 +2,9 @@
 
 backstop reference
 
-curl -s -u admin:admin \
-  -Fjcr:title="Append" \
-  http://localhost:4502/content/platform/ccl/en/components/jcr:content
+curl -s -u admin:admin -X POST \
+     -Fjcr:title@Patch=true -Fjcr:title=+" - Sling" --url http://localhost:4502/content/platform/ccl/en/components/jcr:content
 
-curl -s -u admin:admin \
-  -X POST -F path="/content/platform/ccl/en/components" -F \
-  cmd="activate" http://localhost:4502/bin/replicate.json
+curl -u admin:admin -X POST -F path="/content/platform/ccl/en/components" -F cmd="activate" http://localhost:4502/bin/replicate.json
 
 backstop test
