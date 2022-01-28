ff4651fed8675b4c655c16897abd403388954af0
Trincu, Dan <dan.trincu@pearson.com>
2022-01-28 09:40:47 +0200

    Final updates - All done

--- AEM-Sling-and-Curl/script.sh
+++ AEM-Sling-and-Curl/script.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-backstop reference
+#backstop reference
 
-curl -s -u admin:admin -X POST \
-     -Fjcr:title@Patch=true -Fjcr:title=+" - Sling" --url http://localhost:4502/content/platform/ccl/en/components/jcr:content
+PATHS=`curl -u admin:admin http://localhost:4502/bin/querybuilder.json\?type\=cq:Page\&path\=/content/platform/ccl/en/components\&p.limit=-1 | jq -r '.hits[] | .path'`
+for path in ${PATHS[@]}; do
+  oldTitle=`curl -s -u admin:admin --url http://localhost:4502/${path}/jcr:content.json | jq .'"jcr:title"'`
+  oldTitle=$(sed -e 's/^"//' -e 's/"$//' <<<"$oldTitle")
+	newTitle="$oldTitle - Sling"
+	curl -u admin:admin -X POST -F condition="" -F"jcr:title=${newTitle}" --url "http://localhost:4502${path}/jcr:content"
+	echo "** Now changeing http://localhost:4502${path}/jcr:content **"
+	echo "======================"
+	echo "PATH: ${path}"
+	echo "Old title: ${oldTitle}"
+	echo "New title: ${newTitle}"
+	echo "======================"
+done
+#curl -s -u admin:admin -X POST -F path="/content/platform/ccl/en/components" -F cmd="activate" http://localhost:4502/bin/replicate.json
 
-curl -u admin:admin -X POST -F path="/content/platform/ccl/en/components" -F cmd="activate" http://localhost:4502/bin/replicate.json
+#backstop test
 
-backstop test
+
+#curl -s -u admin:admin -X POST -F condition="" -Fjcr:title=test --url http://localhost:4502/content/platform/ccl/en/components/breadcrumbs/jcr:content
